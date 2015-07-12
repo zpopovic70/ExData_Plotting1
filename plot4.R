f8a3ab6704fff7bbfc6cf039cf05a3a436764b87
zpopovic70 <popovicz70@gmail.com>
2015-07-13 00:38:37 +0100

    Final version

--- plot4.R
+++ plot4.R
@@ -26,16 +26,16 @@ print ("Creating Plot4")
 par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
 with(hpc2days, {
     plot(Global_active_power~DateTime, 
-         xlab="", ylab="Global Active Power (kilowatts)", type="l")
+         xlab="", ylab="Global Active Power", type="l")
     plot(Voltage~DateTime,  
-         xlab="", ylab="Voltage (volt)", type="l")
+         xlab="datetime", ylab="Voltage", type="l")
     plot(Sub_metering_1~DateTime,
-         xlab="", ylab="Global Active Power (kilowatts)", type="l")
+         xlab="", ylab="Energy sub metering", type="l")
     lines(Sub_metering_2~DateTime,col='Red')
     lines(Sub_metering_3~DateTime,col='Blue')
     legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
            legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
-    plot(Global_reactive_power~DateTime, xlab="", ylab="Global Rective Power (kilowatts)", type="l")
+    plot(Global_reactive_power~DateTime, xlab="datetime", ylab="Global_reactive_power", type="l")
     })
 
 # Close the output device
